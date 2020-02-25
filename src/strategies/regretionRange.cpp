65c40db55df3f5b2ebfd2d3d3a34e95b6d3dbf94
Adan J. Suarez <adan@zeitspace.com>
2020-02-25 17:05:25 -0500

    Added quoteEvent to RegretionRange

--- src/strategies/regretionRange.cpp
+++ src/strategies/regretionRange.cpp
@@ -3,17 +3,33 @@
 /**
  * @brief  Generate an instance of RegretionRange
  * @note
- * @param  data: const Data&
+ * @param numOfEntry
+ * @param entryDistance
+ * @param lot
+ * @param inventory
  * @retval Constructor
  */
-RegretionRange::RegretionRange(const Data& data){
-    m_data = data;
+RegretionRange::RegretionRange(int numOfEntry, double entryDistance, double lot, double inventory) {
+    m_numOfEntries = numOfEntry;
+    m_entryDistance = entryDistance;
+    m_lot = lot;
+    m_inventory = inventory;
 };
 /**
  * @brief  Get result of the strategy.
  * @note
- * @retval double
+ * @retval RawDataLine
  */
-double RegretionRange::getResult() {
-    return m_result;
+void RegretionRange::quoteEvent(const RawDataLine& quoteEvent) {
+    /* TODO:
+    Check if it's needed to update parameters.
+    Check if only allowed trigger out.
+    Check if trigger in:
+        Add to result.
+        Add to inventory.
+        Add to trigger out.
+        Check inventory, if > max_inventory, allow only trigger out.
+    Check if trigger out:
+        Calculate result.
+    */
 }
--- src/strategies/regretionRange.h
+++ src/strategies/regretionRange.h
@@ -4,10 +4,17 @@ class RegretionRange {
 
     int m_numOfEntries {3};
     double m_entryDistance {0.1};
-    Data m_data;
-    double m_result;
+    double m_lot {0.01};
+    double m_inventory {0};
+    const double m_max_inventory {m_lot * 3};
 
     public:
-        RegretionRange(const Data&);
-        double getResult();
-}
+        RegretionRange(int numOfEntry, double entryDistance, double lot, double inventory);
+        void quoteEvent(const RawDataLine& quoteEvent);
+        // double getResult(); Not in here!
+    private:
+        void updateParameters();
+        bool triggerIn();
+        bool triggerOut();
+        bool maxInventory();
+};

76bd6661226e31123a74002d00f8505f9cf98018
Adan J. Suarez <adan@zeitspace.com>
2020-02-26 14:13:17 -0500

    Stubed all methods and documentation

--- src/strategies/regretionRange.cpp
+++ src/strategies/regretionRange.cpp
@@ -21,7 +21,7 @@ RegretionRange::RegretionRange(int numOfEntry, double entryDistance, double lot,
  * @retval RawDataLine
  */
 void RegretionRange::quoteEvent(const RawDataLine& quoteEvent) {
-    /* TODO:
+    /*
     Check if it's needed to update parameters.
     Check if only allowed trigger out.
     Check if trigger in:
@@ -32,4 +32,46 @@ void RegretionRange::quoteEvent(const RawDataLine& quoteEvent) {
     Check if trigger out:
         Calculate result.
     */
+   updateParameters();
+   if (!triggerIn()) {
+       triggerOut();
+   };
+}
+/**
+ * @brief  Check if the namber of tick is bigger than the range and if so update parameters.
+ * @note
+ * @retval None
+ */
+void RegretionRange::updateParameters() {
+    /*
+    Check the number of ticks have been pass, if it
+    pass that number, update distance.
+
+    If it doesn't, do nothing.
+    */
+}
+/**
+ * @brief  Simulate send an order, and update result, inventory and trigger out.
+ * @note
+ * @retval bool
+ */
+bool RegretionRange::triggerIn() {
+    /*
+    Add to result
+    Add to inventory.
+    Add to trigger out.
+    Check inventory, if > max_inventory, allow only trigger out.
+    */
+    return false;
+}
+/**
+ * @brief  Check if trigger out, and if so, calculate result.
+ * @note
+ * @retval
+ */
+bool RegretionRange::triggerOut() {
+    /*
+    Calculate result.
+    */
+    return false;
 }
